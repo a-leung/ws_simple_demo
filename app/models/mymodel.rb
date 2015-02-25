8ecbc505d7dd106c3361171959d4646ef7e5ee1f
Andrew Leung <andrew@key.me>
2015-02-25 12:48:50 -0500

    privatizing status and time methods on mymodel

--- app/models/mymodel.rb
+++ app/models/mymodel.rb
@@ -1,16 +1,17 @@
 class Mymodel # < ActiveRecord::Base
 
+  def publish_to_channel
+    details = { 'checkin_time' => checkin_time,
+                'status' => status }
+    WebsocketRails[:special_channel].trigger(:new_message, details.to_json)
+  end
+
+  private
   def status
     %w(good bad ugly).sample
   end
 
   def checkin_time
     Time.zone.now.to_s
-  end
-
-  def publish_to_channel
-    details = { 'checkin_time' => checkin_time,
-                'status' => status }
-    WebsocketRails[:special_channel].trigger(:new_message, details.to_json)
   end
 end
